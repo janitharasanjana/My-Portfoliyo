ff2f496b5e02d35a54db87cb1129d5134950d355
janitharasanjana <janitharasanjana59@gmail.com>
2022-12-02 13:19:15 +0530

    update and crete animation to the ninja

--- assignments/js/game/assets/js/game.js
+++ assignments/js/game/assets/js/game.js
@@ -1,11 +1,21 @@
 var boy = document.getElementById("boy");
-idleImageNumber=0
+
+
+idleImageNumber= 0
+idelAnimationNumber = 0
+
+
 function idleAnimation (){
-    idleImageNumber = idleImageNumber = 1 ;
-    if (idleImageNumber==10) {
-        idleImageNumber = 1 ;
+    idleImageNumber = idleImageNumber + 1 ;
+    if (idleImageNumber == 10) {
+        idleImageNumber = 0 ;
     }
 
 
-    boy.src = "assets/image/sprites/ninjaadventurenew/png/idle (" + idleImageNumber + ").png";
+    boy.src = "assets/image/idle(" + idleImageNumber + ").png";
+}
+
+
+function idleAnimationStart(){
+    idelAnimationNumber = setInterval(idleAnimation,200);
 }
