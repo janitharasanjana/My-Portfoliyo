0eb6828aa0ec0708eff55f67e915d0e6c8e5d82d
janitharasanjana <janitharasanjana59@gmail.com>
2022-12-10 13:25:30 +0530

    create run animation

--- assignments/js/game/assets/js/game.js
+++ assignments/js/game/assets/js/game.js
@@ -19,3 +19,37 @@ function idleAnimation (){
 function idleAnimationStart(){
     idelAnimationNumber = setInterval(idleAnimation,50);
 }
+
+
+runImageNumber = 0;
+runAnimationNumber = 0;
+
+function runAnimation(){
+    runImageNumber = runImageNumber + 1 ;
+
+    if (runImageNumber == 10){
+        runImageNumber = 0 ;
+    }
+    boy.src = "assets/image/run(" + runImageNumber + ").png";
+
+}
+
+
+function runAnimationStart(){
+    runAnimationNumber = setInterval(runAnimation,100);
+    clearInterval(idelAnimationNumber);
+
+}
+
+function keyCheck(event){
+
+   /* alert(event.which);
+    enter = 13*/
+
+    var keyCode = event.which;
+    if (keyCode == 13){
+        if (runAnimationNumber == 0){
+            runAnimationStart();
+        }
+    }
+}
